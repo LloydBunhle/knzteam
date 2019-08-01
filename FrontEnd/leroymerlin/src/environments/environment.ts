3f22c168fd272eddd7df930f86ff8e442d0ed7bf
Blackcaucasion <lteencwadi@gmail.com>
2019-08-01 15:24:25 +0200

    project

---
+++ FrontEnd/leroymerlin/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  apiKey: "AIzaSyAeI-lOahlg2h-mIi2maXtHrHLRO5Sd1CM",
+  authDomain: "leroymerlin-11bf6.firebaseapp.com",
+  databaseURL: "https://leroymerlin-11bf6.firebaseio.com",
+  projectId: "leroymerlin-11bf6",
+  storageBucket: "",
+  messagingSenderId: "696400856270",
+  appId: "1:696400856270:web:d1b9f9685d5ea95b",
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
