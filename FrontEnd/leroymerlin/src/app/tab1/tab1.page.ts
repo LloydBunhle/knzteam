c6475b50ab1963d57122cb272b5e1356a6255513
ChweneKM <chwene.karabo10@gmail.com>
2019-08-01 22:38:18 +0200

    Landing page

--- FrontEnd/leroymerlin/src/app/tab1/tab1.page.ts
+++ FrontEnd/leroymerlin/src/app/tab1/tab1.page.ts
@@ -8,42 +8,28 @@ import {IonSlides} from '@ionic/angular';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page  implements OnInit {
 
-  categoryChanged(ev: any) {
-    console.log('category changed', ev);
-  }
 
-//  @ViewChild('slider') slider: IonSlides;
-//  page = "0";
-//  @ViewChild(IonSlides) slides: IonSlides;
 
   items = [];
 
   private data = [
     {
-      category: 'PROMOTION',
-      expanded: true,
-      products: [
-        { id: 0, name: 'Mag Base Drill', description: 'Standard', price: '350' },
-        { id: 1, name: 'Auger Bit', description: '300mm', price: '120' },
-     
-       
-      ]
-    },
-    {
       category: 'BREAKING & DRILLING',
       products: [
-        { id: 0, name: 'Auger', description: 'BT 121', price: '610' },
-        { id: 1, name: 'Drill', description: 'SDS Max', price: '285' },
-        { id: 2, name: 'Jack Hammer', description: 'Large T3000 (Hilti Excl. Chisel)', price: '690' },
+        { id: 1, name: 'Auger', description: 'BT 121', price: '610' },
+        { id: 2, name: 'Drill', description: 'SDS Max', price: '285' },
+        { id: 3, name: 'Jack Hammer', description: 'Large T3000 (Hilti Excl. Chisel)', price: '690' },
       ]
     },
     {
       category: 'ELECTRIC TOOLS',
       products: [
-        { id: 3, name: 'Floor Machine Scarifier', description: 'Small',  price: '780' },
-        { id: 4, name: 'Wall Chaser', description: '(Excl. Blades)',  price: '310' }
+        { id: 4, name: 'Floor Machine Scarifier', description: 'Small',  price: '780' },
+        { id: 5, name: 'Wall Chaser', description: '(Excl. Blades)',  price: '310' }
       ]
     },
     {
@@ -55,13 +41,25 @@ export class Tab1Page  implements OnInit {
       ]
     },
     {
+
       category: 'LAWN & GARDEN',
       products: [
-        { id: 6, name: 'Chain Saw', description: 'MS381',  price: '610' },
-        { id: 7, name: 'Lawn Mower', description: 'Petro',  price: '310' },
+        { id: 8, name: 'Chain Saw', description: 'MS381',  price: '610' },
+        { id: 9, name: 'Lawn Mower', description: 'Petro',  price: '310' },
       
       ]
-    }
+    },
+    // {
+    //   category: 'PROMOTION',
+      
+    //   products: [
+    //     { id: 10, name: 'Mag Base Drill', description: 'Standard', price: '350' },
+    //     { id: 11, name: 'Auger Bit', description: '300mm', price: '120' },
+    //     { id: 12, name: 'Planer', description: '(Incl. Blades)', price: '100' },
+     
+       
+    //   ]
+    // },
   ];
 
   public isSearchbarOpened = false;
@@ -74,12 +72,10 @@ export class Tab1Page  implements OnInit {
 
   constructor() {}
 
-  // selectedTab(index) {
-  //   this.slider.slideTo(index);
-  // }
+  
 
   ngOnInit() {
-    this.getProducts() 
+    this.items = this.getProducts() 
   }
 
   getProducts() {
